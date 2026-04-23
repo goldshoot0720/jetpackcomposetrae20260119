e1d27e352238ccbf7b76d3653b32ccbf29a14ac4
goldshoot0720 <goldshoot0720@gmail.com>
2026-04-23 18:34:58 +0800

    Add FengBro tools menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.jetpackcomposetrae20260119"
         minSdk = 24
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.2.22"
+        versionCode = 26
+        versionName = "1.2.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
